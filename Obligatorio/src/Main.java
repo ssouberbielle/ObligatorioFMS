f59a478adb8f308f7db977715ae18737910a1dc7
tatosouber <ssouberbielle@hotmail.com>
2020-06-10 17:31:13 -0300

    Apaches commons CSV Intento

--- Obligatorio/src/Main.java
+++ Obligatorio/src/Main.java
@@ -1,12 +1,20 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         menu();
     }
 
 
-    public static void menu() {
+    public static void menu() throws IOException {
         boolean run = true;
         while (run) {
             System.out.println("Seleccione la opción que desee");
@@ -16,7 +24,38 @@ public class Main {
             Scanner scanner = new Scanner(System.in);
             int respuesta = scanner.nextInt();
             switch (respuesta) {
-                case 1: // metodo de cargar datos e imprimir
+                case 1:
+                   /* Reader in = new FileReader("Users/tatosouber/Desktop/fuentedatos/books.csv");
+                    Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(in);
+                    for (CSVRecord record : records) {
+                        String book_id = record.get("Last Name");
+                        String firstName = record.get("First Name");
+                    } */
+
+                    Reader in = new FileReader("/Users/tatosouber/Desktop/fuentedatos/books.csv");
+                    Iterable<CSVRecord> records = CSVFormat.RFC4180.parse(in);
+                    for (CSVRecord record : records) {
+                        String bookid = record.get(0);
+                        String isbn = record.get(1);
+                        String authors = record.get(2);
+                        String original_publication_year = record.get(3);
+                        String original_title = record.get(4);
+                        String title = record.get(5);
+                        String language = record.get(6);
+                        String image_url = record.get(7);
+                    }
+
+                   /* final Appendable out =
+                    final CSVPrinter printer = CSVFormat.DEFAULT.withHeader("H1", "H2").print(out); */
+
+
+
+
+
+
+
+
+
                     menu();
                     break;
                 case 2:
