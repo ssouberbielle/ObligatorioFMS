1ba102578103253fdf32b4a6a61b21894b354ab0
Fperdomo2000 <66039600+Fperdomo2000@users.noreply.github.com>
2020-06-14 22:24:49 -0300

    Carga de datos, agrega repetidos los usuarios, y ahora nunca termina

--- Obligatorio/src/LibraryImpl.java
+++ Obligatorio/src/LibraryImpl.java
@@ -1,3 +1,4 @@
+import LinkedList.LinkedList;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 
@@ -9,10 +10,9 @@ import java.io.Reader;
 public class LibraryImpl implements Library {
 
 
-
-    public static Book [] loadBooks() throws IOException, FileNotFoundException {
+    public static Book[] loadBooks() throws IOException, FileNotFoundException {
         int i = 0;
-        Book libros[] = new Book [10001];
+        Book libros[] = new Book[10001];
         Reader in = new FileReader("fuentedatos/books.csv");
         Iterable<CSVRecord> records = CSVFormat.RFC4180.parse(in);
         for (CSVRecord record : records) {
@@ -25,9 +25,23 @@ public class LibraryImpl implements Library {
             String language = record.get(6);
             String image_url = record.get(7);
             //check carga de datos
-            Book book = new Book(book_id, isbn, authors, original_publication_year, original_title, title, language, image_url);
+
+            int year;
+            try {
+                year = Integer.parseInt(original_publication_year);
+            } catch (NumberFormatException e) {
+                year = 0;
+            }
+            long idBook;
+            try {
+                idBook = Integer.parseInt(book_id);
+            } catch (NumberFormatException e) {
+                idBook = 0;
+            }
+
+            Book book = new Book(idBook, isbn, authors, year, original_title, title, language, image_url);
             libros[i] = book;
-            i ++;
+            i++;
         }
         return libros;
 
@@ -59,6 +73,29 @@ public class LibraryImpl implements Library {
     }
 
     public void topTwentyPublication() {
+    }
+
+    public static LinkedList<User> loadUser() throws IOException {
+        LinkedList<User> users = new LinkedList<User>();
+        Reader in = new FileReader("fuentedatos/to_read.csv");
+        Iterable<CSVRecord> records = CSVFormat.RFC4180.parse(in);
+        for (CSVRecord record : records) {
+            String user_id = record.get(0);
+            long idUser;
+            try {
+                idUser = Long.parseLong(user_id);
+            } catch (NumberFormatException e) {
+                idUser = 0;
+            }
+            User user = new User(idUser);
+            if (users.contains(user)) {
+                continue;
+                 // se estan haciendo usuarios repetidos
+            }
+            users.add(user);
+
+        }
+        return users;
 
     }
 }
