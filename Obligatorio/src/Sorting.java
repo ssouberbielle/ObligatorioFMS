7d4534cf38aeb5f0ff38351c11df005aa7f092f4
tatosouber <ssouberbielle@hotmail.com>
2020-06-23 21:38:16 -0300

    Listo.

--- Obligatorio/src/Sorting.java
+++ Obligatorio/src/Sorting.java
@@ -1,54 +1,5 @@
 public class Sorting {
 
-   /* public static <T extends Comparable<T>> void mergeSort (T[] arr, int len){
-        if (len < 2){return;}
-
-        int mid = len / 2;
-        T[] left_arr = (T[]) new Object[mid];
-        T [] right_arr = (T[]) new Object[len-mid];
-
-        //Dividing array into two and copying into two separate arrays
-        int k = 0;
-        for(int i = 0;i<len;++i){
-            if(i<mid){
-                left_arr[i] = arr[i];
-            }
-            else{
-                right_arr[k] = arr[i];
-                k = k+1;
-            }
-        }
-        // Recursively calling the function to divide the subarrays further
-        mergeSort(left_arr,mid);
-        mergeSort(right_arr,len-mid);
-        // Calling the merge method on each subdivision
-        merge(left_arr,right_arr,arr,mid,len-mid);
-    }
-
-    public static <T extends Comparable <T>> void merge(T[] left_arr, T[] right_arr, T[] arr,int left_size, int right_size){
-
-        int i=0,l=0,r = 0;
-        //The while loops check the conditions for merging
-        while(l<left_size && r<right_size){
-
-            if(left_arr[l].compareTo(right_arr[r]) < 0){
-                arr[i++] = left_arr[l++];
-            }
-            else{
-                arr[i++] = right_arr[r++];
-            }
-        }
-        while(l<left_size){
-            arr[i++] = left_arr[l++];
-        }
-        while(r<right_size){
-            arr[i++] = right_arr[r++];
-        }
-    }
-*/
-
-
-    ////////////////////////////////////
     public static void mergeSort(User[] arr, int len) {
         if (len < 2) {
             return;
@@ -84,7 +35,6 @@ public class Sorting {
         AuthorYearPublications[] left_arr = new AuthorYearPublications[mid];
         AuthorYearPublications[] right_arr = new AuthorYearPublications[len - mid];
 
-        //Dividing array into two and copying into two separate arrays
         int k = 0;
         for (int i = 0; i < len; ++i) {
             if (i < mid) {
@@ -94,17 +44,14 @@ public class Sorting {
                 k = k + 1;
             }
         }
-        // Recursively calling the function to divide the subarrays further
         mergeSort(left_arr, mid);
         mergeSort(right_arr, len - mid);
-        // Calling the merge method on each subdivision
         merge(left_arr, right_arr, arr, mid, len - mid);
     }
 
     public static void merge(AuthorYearPublications[] left_arr, AuthorYearPublications[] right_arr, AuthorYearPublications[] arr, int left_size, int right_size) {
 
         int i = 0, l = 0, r = 0;
-        //The while loops check the conditions for merging
         while (l < left_size && r < right_size) {
 
             if (left_arr[l].compareTo(right_arr[r]) < 0) {
@@ -172,7 +119,7 @@ public class Sorting {
     public static void merge(LanguageTimes[] left_arr, LanguageTimes[] right_arr, LanguageTimes[] arr, int left_size, int right_size) {
 
         int i = 0, l = 0, r = 0;
-        //The while loops check the conditions for merging
+
         while (l < left_size && r < right_size) {
 
             if (left_arr[l].compareTo(right_arr[r]) < 0) {
@@ -193,12 +140,9 @@ public class Sorting {
         if (len < 2) {
             return;
         }
-
         int mid = len / 2;
         Book[] left_arr = new Book[mid];
         Book[] right_arr = new Book[len - mid];
-
-        //Dividing array into two and copying into two separate arrays
         int k = 0;
         for (int i = 0; i < len; ++i) {
             if (i < mid) {
@@ -208,10 +152,8 @@ public class Sorting {
                 k = k + 1;
             }
         }
-        // Recursively calling the function to divide the subarrays further
         mergeSort(left_arr, mid);
         mergeSort(right_arr, len - mid);
-        // Calling the merge method on each subdivision
         merge(left_arr, right_arr, arr, mid, len - mid);
     }
 
@@ -219,7 +161,6 @@ public class Sorting {
     public static void merge(Book[] left_arr, Book[] right_arr, Book[] arr, int left_size, int right_size) {
 
         int i = 0, l = 0, r = 0;
-        //The while loops check the conditions for merging
         while (l < left_size && r < right_size) {
 
             if (left_arr[l].compareTo(right_arr[r]) < 0) {
